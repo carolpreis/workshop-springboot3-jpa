9ef358e4bc93356a43292c8c8b2e3f92f643b1da
Carolina Reis <carolinapimentar@gmail.com>
2024-09-14 13:00:39 +0100

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -20,14 +24,21 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<User>> findAll(){	//para ir buscar todos os usuarios
 		List<User> list = service.findAll();		
 		return ResponseEntity.ok().body(list);
 	}
 
-	@GetMapping(value = "/{id}")	//pra dizer que a url vai ter um parametro
+	@GetMapping(value = "/{id}")	//pra dizer que a url vai ter um parametro. a / diz que queremos um usuario em especifico
 	public ResponseEntity<User> findById(@PathVariable Long id){	//para o string aceitar o id e considerar como parametro que vai chegar no url
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}	
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
